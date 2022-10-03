81a7dd26d28f2422e9b91a39e205dd1f8a1dac40
Salman Mehmood <salmanmehmood613@gmail.com>
2022-10-03 14:42:03 +0500

    project setup

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "../Navbar/style.css";
+
+function NavbarHeader() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand className="logo" href="#home">Salman</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto navbarFull">
+            <Nav.Link className="navLink" href="#home">Home</Nav.Link>
+            <Nav.Link className="navLink" href="#about">About</Nav.Link>
+            <Nav.Link className="navLink" href="#services">Services</Nav.Link>
+            <Nav.Link className="navLink" href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link className="navLink" href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarHeader;
